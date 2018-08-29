c81824abdb831a6de17e84bd4d9a2496c47039dc
houzhongjian <1556469348@qq.com>
2018-08-30 00:30:55 +0800

    day1

---
+++ src/com/learn/day1/hello.kt
@@ -0,0 +1,9 @@
+package com.learn.day1
+
+fun main(args: Array<String>) {
+    println("hello kotlin!")
+    var name = "张三"
+    println(name)
+    val gateway: String = "https://www.houzhongjian.com"
+    println("网关地址为:$gateway")
+}
